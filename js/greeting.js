44723beeb431a2235769cafd8237a38f266430fc
dariija <darija.bakanova@gmail.com>
2021-10-19 14:18:27 +0300

    feat: add greeting

---
+++ js/greeting.js
@@ -0,0 +1,31 @@
+const greeting = document.querySelector('.greeting');
+const userName = document.querySelector('.name');
+
+function getTimeOfDay() {
+    let time = new Date().getHours();
+    return ( time < 5.59)? 'night' : 
+           ( time < 11.59)? 'morning' :
+           ( time < 17.59)? 'afternoon' : 'evening'
+};
+
+function showGreeting() {
+    let timeOfDay = getTimeOfDay();
+    greeting.textContent = `Good ${timeOfDay}, `;
+
+    setTimeout(showGreeting, 1000);
+};
+
+function setLocalStorage() {
+    localStorage.setItem('name', userName.value)
+};
+
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        userName.value = localStorage.getItem('name');
+    };
+};
+
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
+
+showGreeting();
