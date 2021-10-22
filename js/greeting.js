191a4ae5c6eabe540accebdcbc489fd1dd0ea5ce
dariija <darija.bakanova@gmail.com>
2021-10-22 12:46:36 +0300

    feat: add code for weather widget

--- js/greeting.js
+++ js/greeting.js
@@ -6,24 +6,29 @@ function getTimeOfDay() {
     return ( time < 5.59)? 'night' : 
            ( time < 11.59)? 'morning' :
            ( time < 17.59)? 'afternoon' : 'evening'
-};
+}
 
 function showGreeting() {
     let timeOfDay = getTimeOfDay();
     greeting.textContent = `Good ${timeOfDay}, `;
 
     setTimeout(showGreeting, 1000);
-};
+}
 
 function setLocalStorage() {
-    localStorage.setItem('name', userName.value)
-};
+    localStorage.setItem('name', userName.value);
+    localStorage.setItem('city', city.value)
+}
 
 function getLocalStorage() {
     if (localStorage.getItem('name')) {
         userName.value = localStorage.getItem('name');
     };
-};
+
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city')
+    };
+}
 
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
