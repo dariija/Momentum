227b02967e5befc529ce3a9508f32a7407332be5
dariija <darija.bakanova@gmail.com>
2021-10-23 22:36:33 +0300

    feat: add audio-player

---
+++ js/playlist.js
@@ -0,0 +1,24 @@
+let playlistAudio = [
+    {
+        name: 'Aqua Caelestis',
+        src: '../assets/sounds/Aqua Caelestis.mp3',
+        duration: '0:39'
+    }, 
+    {
+        name: 'Ennio Morricone',
+        src: '../assets/sounds/Ennio Morricone.mp3',
+        duration: '1:37'
+    }, 
+    {
+        name: 'River Flows In You',
+        src: '../assets/sounds/River Flows In You.mp3',
+        duration: '1:37'
+    }, 
+    {
+        name: 'Summer Wind',
+        src: '../assets/sounds/Summer Wind.mp3',
+        duration: '1:50'
+    }, 
+];
+
+export {playlistAudio};
